f1375805271afe7029ca1ddbe16e98d1e8ebfa2c
ShrikantBorole <shrikant.borole@gmail.com>
2023-06-04 08:46:18 +0530

    Factory Method Design Pattern

--- FactoryMethod/Implementation.cs
+++ FactoryMethod/Implementation.cs
@@ -1,4 +1,92 @@
 ﻿namespace FactoryMethod
 {
-   
+    interface IDiscountService
+    {
+        public int DiscountPercentage();
+    }
+
+    class CountryDiscountService : IDiscountService
+    {
+        string _countryCode;
+        public CountryDiscountService(string countryCode)
+        {
+            _countryCode = countryCode;
+        }
+        public int DiscountPercentage()
+        {
+            switch (_countryCode)
+            {
+                case "BE":
+                    Console.WriteLine("BE");
+                    return 30;
+                case "ME":
+                    Console.WriteLine("ME");
+                    return 20;
+                default:
+                    return 0;
+            }
+        }
+    }
+
+    class CodeDiscountService : IDiscountService
+    {
+        Discount _codeCode;
+        public CodeDiscountService(Discount codeCode)
+        {
+            _codeCode = codeCode;
+        }
+        public int DiscountPercentage()
+        {
+            switch (_codeCode)
+            {
+                case Discount.BigDiscount:
+                    Console.WriteLine("BigDiscount");
+                    return 89;
+                case Discount.SmallDiscount:
+                    Console.WriteLine("SmallDiscount");
+                    return 29;
+                default:
+                    return 0;
+            }
+        }
+    }
+
+    abstract class DiscountFactory
+    {
+        public abstract IDiscountService CreateDiscountService();
+    }
+
+    class CountryDiscountFatory : DiscountFactory
+    {
+        string _countryCode;
+        public CountryDiscountFatory(string countryCode)
+        {
+            _countryCode= countryCode;
+        }
+        public override IDiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryCode);
+        }
+    }
+
+    class CodeDiscountFactory : DiscountFactory
+    {
+        Discount _codeCode;
+
+        public CodeDiscountFactory(Discount codeCode)
+        {
+            _codeCode = codeCode;
+        }
+
+        public override IDiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_codeCode);
+        }
+    }
+
+    enum Discount
+    {
+        BigDiscount,
+        SmallDiscount
+    }
 }
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.Title = "Factory Method";
+﻿using FactoryMethod;
+
+Console.Title = "Factory Method";
+List<DiscountFactory> discountFactories = new List<DiscountFactory>();
+discountFactories.Add(new CountryDiscountFatory("BE"));
+discountFactories.Add(new CodeDiscountFactory(Discount.BigDiscount));
+
+foreach (var item in discountFactories)
+{
+    var discountService = item.CreateDiscountService();
+    discountService.DiscountPercentage();
+}
