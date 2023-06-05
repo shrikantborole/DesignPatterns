0b2268338e21bde7d6ebadd88b6e5a4e4841c034
ShrikantBorole <shrikant.borole@gmail.com>
2023-06-05 08:55:39 +0530

    Done with AbstractFactory

--- AbstractFactory/Implementation.cs
+++ AbstractFactory/Implementation.cs
@@ -1,4 +1,92 @@
 ﻿namespace AbstractFactory
 {
-   
+    public interface IDiscountService
+    {
+        public int DiscountPercentage();
+    }
+
+    public class MaharashtraDiscountService : IDiscountService
+    {
+        public int DiscountPercentage()
+        {
+            return 20;
+        }
+    }
+
+    public class GujrathDiscountService : IDiscountService
+    {
+        public int DiscountPercentage()
+        {
+            return 25;
+        }
+    }
+    public interface IShippingCostService
+    {
+        public decimal ShippingCost();
+    }
+
+    public class MaharashtraShippingCostService : IShippingCostService
+    {
+        public decimal ShippingCost()
+        {
+            return 10;
+        }
+    }
+
+    public class GujrathShippingCostService : IShippingCostService
+    {
+        public decimal ShippingCost()
+        {
+            return 5;
+        }
+    }
+
+    public interface IShoppingCartPurchaseFactory
+    {
+        public IShippingCostService CreateShippingCostService();
+        public IDiscountService CreateDiscountService();
+    }
+
+    public class MaharashtraShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+    {
+        public IDiscountService CreateDiscountService()
+        {
+            return new MaharashtraDiscountService();
+        }
+
+        public IShippingCostService CreateShippingCostService()
+        {
+            return new MaharashtraShippingCostService();
+        }
+    }
+
+    public class GujrathShoppingCartPurchaseFactory : IShoppingCartPurchaseFactory
+    {
+        public IDiscountService CreateDiscountService()
+        {
+            return new GujrathDiscountService();
+        }
+
+        public IShippingCostService CreateShippingCostService()
+        {
+            return new GujrathShippingCostService();
+        }
+    }
+
+    public class ShopingCartService
+    {
+        private readonly IShippingCostService _shippingCostService;
+        private readonly IDiscountService _discountService;
+        private readonly int totalCost = 500;
+        public ShopingCartService(IShoppingCartPurchaseFactory shoppingCartPurchaseFactory)
+        {
+            _shippingCostService = shoppingCartPurchaseFactory.CreateShippingCostService();
+            _discountService = shoppingCartPurchaseFactory.CreateDiscountService();
+        }
+        public int Calculate()
+        {
+            var amountForDiscount = (totalCost * _discountService.DiscountPercentage()) / 100;
+            return totalCost - amountForDiscount - Convert.ToInt32(_shippingCostService.ShippingCost());
+        }
+    }
 }
--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Title = "Abstract Factory";
+﻿using AbstractFactory;
+
+Console.Title = "Abstract Factory";
+
+IShoppingCartPurchaseFactory shoppingCartPurchaseFactory = new MaharashtraShoppingCartPurchaseFactory();
+ShopingCartService shopingCartService1 =
+    new ShopingCartService(shoppingCartPurchaseFactory);
+Console.WriteLine(shopingCartService1.Calculate());
+
+shoppingCartPurchaseFactory = new GujrathShoppingCartPurchaseFactory();
+ShopingCartService shopingCartService2 =
+    new ShopingCartService(shoppingCartPurchaseFactory);
+Console.WriteLine(shopingCartService2.Calculate());
+
+Console.WriteLine("Press key to exit !!");
+
