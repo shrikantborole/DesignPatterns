fdfc1b92f59a7ea6887693b2c1f4f623676e8609
ShrikantBorole <shrikant.borole@gmail.com>
2023-07-16 08:59:11 +0530

    Done with the Adapter Design pattern example

---
+++ Adapter/Implementation.cs
@@ -0,0 +1,69 @@
+﻿namespace Adapter
+{
+    public class Implementation
+    {
+        /// <summary>
+        /// Adapter
+        /// </summary>
+        public class CityAdapter : ICityAdapter
+        {
+            public ExternalSystem ExternalSystem { get; private set; } = new();
+
+            public City GetCity()
+            {
+                // call into the external system 
+                var cityFromExternalSystem = ExternalSystem.GetCity();
+
+                return new City(cityFromExternalSystem.Name, cityFromExternalSystem.Inhabitants);
+            }
+        }
+
+        public class CityFromExternalSystem
+        {
+            public string Name { get; private set; }
+            public string NickName { get; private set; }
+            public int Inhabitants { get; private set; }
+
+            public CityFromExternalSystem(
+                string name,
+                string nickName,
+                int inhabitants)
+            {
+                Name = name;
+                NickName = nickName;
+                Inhabitants = inhabitants;
+            }
+        }
+
+        /// <summary>
+        /// Adaptee
+        /// </summary>
+        public class ExternalSystem
+        {
+            public CityFromExternalSystem GetCity()
+            {
+                return new CityFromExternalSystem("Antwerp", "'t Stad", 500000);
+            }
+        }
+
+        public class City
+        {
+            public string FullName { get; private set; }
+            public long Inhabitants { get; private set; }
+
+            public City(string fullName, long inhabitants)
+            {
+                FullName = fullName;
+                Inhabitants = inhabitants;
+            }
+        }
+
+        /// <summary>
+        /// Target
+        /// </summary>
+        public interface ICityAdapter
+        {
+            City GetCity();
+        }
+    }
+}
--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.Title = "Adapter";
+﻿using static Adapter.Implementation;
+
+Console.Title = "Adapter";
+// Adpater design pattern is used to convert/typecase one model to other model.
+// Suppose, you are calling to the external system, and it's returning you information which is not
+// of type that you are using into your code, then you can write a ADAPTER which will convert the 
+// external model into your one.
+
+//In below example, we converted the CityFromExternalSystem to City mode, which is getting used into our code.
+
+ICityAdapter cityAdapter = new CityAdapter();
+var city = cityAdapter.GetCity();
+Console.WriteLine($"City {city.FullName} - {city.Inhabitants}");
