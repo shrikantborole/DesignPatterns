67e8f08c2fe1db9790885f598737be4f91497632
ShrikantBorole <shrikant.borole@gmail.com>
2023-07-19 08:37:59 +0530

    Done with Strategy Pattern

--- Strategy/Implementation.cs
+++ Strategy/Implementation.cs
@@ -1,3 +1,49 @@
 ﻿namespace Strategy
 {
+	public interface IExportService
+	{
+		public void Export(Order order);
+	}
+
+    public class Order
+    {
+        public Order(int orderId, string orderName)
+        {
+            OrderId = orderId;
+            OrderName = orderName;
+        }
+		public int OrderId { get; set; }
+		public string? OrderName { get; set; }
+
+		public void Export(IExportService service)
+		{
+			if (service == null)
+				throw new ArgumentException();
+			service.Export(this);
+		}
+	}
+
+    public class CSVExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+			Console.WriteLine($"CSVExportService => {order.OrderId} {order.OrderName}");
+        }
+    }
+
+    public class XMLExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"XMLExportService => {order.OrderId} {order.OrderName}");
+        }
+    }
+
+    public class JSONExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"JSONExportService => {order.OrderId} {order.OrderName}");
+        }
+    }
 }
--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.Title = "Strategy";
+﻿using Strategy;
+Console.Title = "Strategy";
+
+Order order= new Order(1,"Apple-Order");
+order.Export(new CSVExportService());
+Console.WriteLine("-------------------------");
+order.Export(new XMLExportService());
+Console.WriteLine("-------------------------");
+order.Export(new JSONExportService());
