17311fab38ed08b1ef7f10eeaf4037d1da200548
ShrikantBorole <shrikant.borole@gmail.com>
2023-07-19 07:48:53 +0530

    Added Changes for the Template Method Pattern

--- TemplateMethod/Implementation.cs
+++ TemplateMethod/Implementation.cs
@@ -1,3 +1,66 @@
 ﻿namespace TemplateMethod
 { 
+    public abstract class MailParser    {
+        public virtual void FindServer()
+        {
+            Console.WriteLine("MailParser.FindServer()");
+        }
+
+        public abstract bool AuthenticateServer();
+
+        // If we are confrim that the logic will not get change, then we remove virtual
+        // keyword. In the below case we will able to override the method in the base class
+        public virtual void ParseHtmlBody(string htmlBodyIdentifier)
+        {
+            Console.WriteLine($"MailParser.ParseHtmlBody() => {htmlBodyIdentifier}");
+        }
+        public void ParseMailBody(string mailBodyIdentifier)
+        {
+            FindServer();
+            if (AuthenticateServer())
+            {
+                ParseHtmlBody(mailBodyIdentifier);
+                Console.WriteLine($"MailParser.ParseMailBody() => {mailBodyIdentifier}");
+            }
+        }
+    }
+
+    public class AzureEmailParser : MailParser
+    {
+        public override bool AuthenticateServer()
+        {
+            Console.WriteLine("AzureEmailParser.AuthenticateServer()");
+            return true;
+        }
+    }
+
+    public class AmazonEmailParser : MailParser
+    {
+        public override void ParseHtmlBody(string htmlBodyIdentifier)
+        {
+            Console.WriteLine($"AmazonEmailParser.ParseHtmlBody() => {htmlBodyIdentifier}");
+        }
+        public override bool AuthenticateServer()
+        {
+            Console.WriteLine("AmazonEmailParser.AuthenticateServer()");
+            return true;
+        }
+    }
+
+    public class GoogleEmailParser : MailParser
+    {
+        public override void ParseHtmlBody(string htmlBodyIdentifier)
+        {
+            Console.WriteLine($"GoogleEmailParser.ParseHtmlBody() => {htmlBodyIdentifier}");
+        }
+        public override bool AuthenticateServer()
+        {
+            Console.WriteLine("GoogleEmailParser.AuthenticateServer()");
+            return true;
+        }
+        public override void FindServer()
+        {
+            Console.WriteLine("GoogleEmailParser.FindServer()");
+        }
+    }
 }
--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.Title = "Template Method";
+﻿using TemplateMethod;
+
+Console.Title = "Template Method";
+
+MailParser googleEmailParser = new GoogleEmailParser();
+googleEmailParser.ParseMailBody("ff3f5f39-02df-48fc-9c20-fe115ed106be");
+Console.WriteLine("--------------------------------");
+MailParser amazonEmailParser = new AmazonEmailParser();
+amazonEmailParser.ParseMailBody("b674ee65-5c07-45f2-8c2c-f333d2b1d169");
+Console.WriteLine("--------------------------------");
+MailParser azureEmailParser = new AzureEmailParser();
+azureEmailParser.ParseMailBody("12aacaff-0db3-4231-8923-bc60c5f27f0a");
