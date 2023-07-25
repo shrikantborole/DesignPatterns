c1c5d6b8a7fec5da4bfc4e6915a0e749dc01817b
ShrikantBorole <shrikant.borole@gmail.com>
2023-07-25 08:55:52 +0530

    Observer Pattern

--- Observer/Implementation.cs
+++ Observer/Implementation.cs
@@ -1,3 +1,77 @@
 ﻿namespace Observer
 {
+	public class Employee
+	{
+		public string Name { get; set; }
+		public string DOB { get; set; }
+	}
+
+    /// <summary>
+    /// Observer
+    /// </summary>
+    public interface ISubCompanyBranchChangeListener
+    {
+        void ReceiveEmployeUpdateNotification(Employee employee);
+    }
+
+    /// <summary>
+    /// Observer Implementation
+    /// </summary>
+    public class AundCompanyBranch : ISubCompanyBranchChangeListener
+    {
+        public void ReceiveEmployeUpdateNotification(Employee employee)
+        {
+            Console.WriteLine($"Company Branch {nameof(AundCompanyBranch)} notified, for Employee Name:- {employee.Name}");
+        }
+    }
+    /// <summary>
+    /// Observer Implementation
+    /// </summary>
+    public class WakadCompanyBranch : ISubCompanyBranchChangeListener
+    {
+        public void ReceiveEmployeUpdateNotification(Employee employee)
+        {
+            Console.WriteLine($"Company Branch {nameof(WakadCompanyBranch)} notified, for Employee Name:- {employee.Name}");
+        }
+    }
+
+    /// <summary>
+    /// Class to add/remove observer and notify them
+    /// </summary>
+    public abstract class EmployeeUpdateNotifier
+    {
+        private List<ISubCompanyBranchChangeListener> _observers = new();
+
+        public void AddObserver(ISubCompanyBranchChangeListener subCompanyBranchChangeListener)
+        {
+            _observers.Add(subCompanyBranchChangeListener);
+        }
+
+        public void RemoveObserver(ISubCompanyBranchChangeListener subCompanyBranchChangeListener)
+        {
+            _observers.Remove(subCompanyBranchChangeListener);
+        }
+
+        public void Notify(Employee employee)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.ReceiveEmployeUpdateNotification(employee);
+            }
+        }
+    }
+    public class EmployeeUpdate : EmployeeUpdateNotifier
+    {
+        public void Resign(Employee employee)
+        {
+            Console.WriteLine($"Employee resigned, {employee.Name}");
+            Notify(employee);
+        }
+
+        public void Joined(Employee employee)
+        {
+            Console.WriteLine($"Employee joined, {employee.Name}");
+            Notify(employee);
+        }
+    }
 }
--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.Title = "Observer";
+﻿using Observer;
+
+Console.Title = "Observer";
+
+AundCompanyBranch aundCompanyBranch = new AundCompanyBranch();
+WakadCompanyBranch wakadCompanyBranch= new WakadCompanyBranch();
+
+EmployeeUpdate employeeUpdate = new EmployeeUpdate();
+employeeUpdate.AddObserver(aundCompanyBranch);
+employeeUpdate.AddObserver(wakadCompanyBranch);
+
+employeeUpdate.Resign(new Employee() { Name = "ABC", DOB = "15May91" });
+employeeUpdate.Joined(new Employee() { Name = "XYZ", DOB = "15May98" });
+Console.WriteLine();
+
