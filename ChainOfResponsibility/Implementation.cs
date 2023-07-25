bff47bd706d4dc5ed3dc56b3f4fc23c187f8a6ca
ShrikantBorole <shrikant.borole@gmail.com>
2023-07-25 08:22:57 +0530

    Added Code for the COR pattern

--- ChainOfResponsibility/Implementation.cs
+++ ChainOfResponsibility/Implementation.cs
@@ -1,3 +1,92 @@
 ﻿namespace ChainOfResponsibility
 {
+    public class Document
+    {
+        public string? Name { get; set; }
+        public string Title { get; set; }
+        public string ApprovedBy { get; set; }
+
+        public Document(string name, string title, string approvedBy)
+        {
+            Name = name;
+            Title = title;
+            ApprovedBy = approvedBy;
+        }
+    }
+
+    //Handler
+    public interface IHandler<T> where T : class
+    {
+        IHandler<T> SetSuccessor(IHandler<T> successor);
+        void Handle(T request);
+    }
+
+    public class DocumentNameHandler : IHandler<Document>
+    {
+        private IHandler<Document>? _successor;
+
+        public void Handle(Document document)
+        {
+            if (document.Title == null)
+            {
+                throw new Exception("Titile is Empty");
+                //You can add errors in the Error array and collect it at last, in case
+                //if you don't want to break anything
+            }
+            // go to the next handler
+            _successor?.Handle(document);
+        }
+
+        public IHandler<Document> SetSuccessor(IHandler<Document> successor)
+        {
+            _successor = successor;
+            return successor;
+        }
+    }
+
+    public class DocumentTitleHandler : IHandler<Document>
+    {
+        private IHandler<Document>? _successor;
+
+        public void Handle(Document document)
+        {
+            if (document.Name == null)
+            {
+                throw new Exception("Name is Empty");
+                //You can add errors in the Error array and collect it at last, in case
+                //if you don't want to break anything
+            }
+            // go to the next handler
+            _successor?.Handle(document);
+        }
+
+        public IHandler<Document> SetSuccessor(IHandler<Document> successor)
+        {
+            _successor = successor;
+            return successor;
+        }
+    }
+
+    public class ApprovedByHandler : IHandler<Document>
+    {
+        private IHandler<Document>? _successor;
+
+        public void Handle(Document document)
+        {
+            if (document.ApprovedBy == null)
+            {
+                throw new Exception("ApprovedBy is Empty");
+                //You can add errors in the Error array and collect it at last, in case
+                //if you don't want to break anything
+            }
+            // go to the next handler
+            _successor?.Handle(document);
+        }
+
+        public IHandler<Document> SetSuccessor(IHandler<Document> successor)
+        {
+            _successor = successor;
+            return successor;
+        }
+    }
 }
--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.Title = "Chain of Responsibility";
+﻿using ChainOfResponsibility;
+
+Console.Title = "Chain of Responsibility";
+
+Document _validDocument = new Document("Name","Title","ApprovedBy");
+Document _inValidDocument = new Document(null, "Title", "ApprovedBy");
+
+var _documentHandlerChain = new DocumentTitleHandler();
+_documentHandlerChain.SetSuccessor(new DocumentNameHandler())
+                    .SetSuccessor(new ApprovedByHandler());
+
+try
+{
+    _documentHandlerChain.Handle(_validDocument);
+    Console.WriteLine("Valid document is valid.");
+    _documentHandlerChain.Handle(_inValidDocument);
+    Console.WriteLine("Invalid document is valid.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception occured {ex.Message}");
+}
